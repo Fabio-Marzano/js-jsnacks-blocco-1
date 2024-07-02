11a8ea977983b5eaecf0601094e02b5e2525dea0
fabiomarzano <fabiomarzano1993@gmail.com>
2024-07-02 17:52:01 +0200

    [UPD] create-cicle-for-array

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//CREO L'ARRAY PER I DATI DELL'UTENTE//
+let guets = [Alessandro Del piero, David Trezeguet, Paulo Dybala, Leonardo Bonucci,];
+
+//RECUPERO IL PULSANTE DAL DOM E LO LASCIO IN ATTESA DI EVENTO CLICK//
+
+document.getElementById('check-guest').addEventListener('click', function() {
+        //RECUPERO IL VALORE INSERITO NELL'INPUT//
+    let name = document.getElementById('name').value;
+    //GENERO CICLO ARRAY//
+    for (let i = 0; i < guets.length; i++) {
+        
+        if(guests[i].toLoverCase() === name.toLoverCase) {
+    }
+
+});
