10ac7addf7831f5dba8118037e0d7028ef2ce548
fabiomarzano <fabiomarzano1993@gmail.com>
2024-07-02 19:41:45 +0200

    [UPD] verified-name-in-console

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
 //CREO L'ARRAY PER I DATI DELL'UTENTE//
-let guets = [Alessandro Del piero, David Trezeguet, Paulo Dybala, Leonardo Bonucci,];
+let guests = ['Alessandro Del piero', 'David Trezeguet', 'Paulo Dybala', 'Leonardo Bonucci'];
+//IMPOSTO VARIABILE DI CONTROLLO//
+let check_guest = false;
 
 //RECUPERO IL PULSANTE DAL DOM E LO LASCIO IN ATTESA DI EVENTO CLICK//
 
-document.getElementById('check-guest').addEventListener('click', function() {
+document.getElementById('check-guest').addEventListener('click', function () {
         //RECUPERO IL VALORE INSERITO NELL'INPUT//
     let name = document.getElementById('name').value;
     //GENERO CICLO ARRAY//
-    for (let i = 0; i < guets.length; i++) {
+    for (let i = 0; i < guests.length; i++) {
+        //CONTROLLO VALORE ITERATO//
+        if (guests[i].toLowerCase() === name.toLowerCase()) {
+            check_guest = true;
+        }   
+    }
         
-        if(guests[i].toLoverCase() === name.toLoverCase) {
+        
+    //VERIFICO VARIABILE DI CONTROLLO//
+    if (check_guest) {
+        console.log(`Benvenuto ${name}, prego si accomodi`);
+    }
+
+    else {
+        console.log(`ALT! Chi siete, non entrate`);
     }
 
 });
